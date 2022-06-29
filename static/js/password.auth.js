b366a8e5b7245bb7bdaf9664c4df09529176f802
Venkata Vamsy <79786849+vvamsy@users.noreply.github.com>
2022-06-29 13:56:31 +0530

    Update password.auth.js
    Updating submit event - Working - Custom Protocol

--- static/js/password.auth.js
+++ static/js/password.auth.js
@@ -30,36 +30,16 @@
 //         }
 //     })
 // })
+$('#login').submit(function(event) {
+    event.preventDefault();
 
-// /* Handle for login form submission */
-// $('#login').submit(function(event) {
-//     event.preventDefault();
+    let username = this.username.value;
+    if(username === ""){
+       alert("Please fill username");
+       return;
+    }
 
-//     let username = this.username.value;
-//     let password = this.password.value;
-
-//     if(!username || !password) {
-//         alert('Username or password is missing!')
-//         return
-//     }
-
-//     let formBody = {username, password}; 
-//     fetch('/password/login', {
-//         method: 'POST',
-//         credentials: 'include',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(formBody)
-//     })
-//     .then((response) => response.json())
-//     .then((response) => {
-//         if(response.status === 'ok') {
-//             loadMainContainer()   
-//         } else {
-//             alert(`Server responed with error. The message is: ${response.message}`);
-//         }
-//     })
-// })
-
-
+    let formBody = {username}; 
+    let jFormBody = JSON.stringify(formBody);
+    window.location.assign("f5access://Fido2_done?" + jFormBody);
+})
